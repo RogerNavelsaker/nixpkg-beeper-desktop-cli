9a4edd2b50413051aff55df5ccb2e4df9aaba549
Roger Navelsaker <roger.navelsaker@gmail.com>
2026-06-16 18:43:56 +0200

    fix: use pipe delimiter in sed to avoid slash conflicts

--- update.sh
+++ update.sh
@@ -12,8 +12,8 @@ echo "Updating to version $version..."
 hash=$(nix-prefetch-url --unpack "https://github.com/beeper/desktop-api-cli/archive/refs/tags/$latest_tag.tar.gz" 2>/dev/null)
 sri_hash=$(nix hash to-sri --type sha256 "$hash")
 
-sed -i "s/version = \".*\"/version = \"$version\"/" flake.nix
-sed -i "s/rev = \".*\"/rev = \"$latest_tag\"/" flake.nix
-sed -i "s/hash = \"sha256-.*\"/hash = \"$sri_hash\"/" flake.nix
+sed -i "s|version = \".*\"|version = \"$version\"|" flake.nix
+sed -i "s|rev = \".*\"|rev = \"$latest_tag\"|" flake.nix
+sed -i "s|hash = \"sha256-.*\"|hash = \"$sri_hash\"|" flake.nix
 
 echo "Done."
